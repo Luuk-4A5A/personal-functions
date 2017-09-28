259b619b0b25eb48ca1541f24ef498f839b53454
unknown <501287@edu.rocmn.nl>
2017-09-28 11:20:30 +0200

    updated form data function

--- javascript/formdata/index.php
+++ javascript/formdata/index.php
@@ -11,9 +11,10 @@
       <textarea data-form-information="textarea">dit is content denk ik</textarea>
       <select data-form-information="select">
         <option>1</option>
-        <option selected>2</option>
-        <option>3</option>
+        <option>2</option>
+        <option selected>3</option>
       </select>
+      <input data-form-information-file="file" type="text" value='hoi'>
     </form>
     <script src="main.js"></script>
   </body>
--- javascript/formdata/main.js
+++ javascript/formdata/main.js
@@ -2,17 +2,20 @@ function gatherFormData(formTagName, callback) {
   let string = '';
   let formData = document.querySelectorAll(formTagName);
   let cleanFormTagName = formTagName.slice(1, -1);
+
   for(var i = 0; i < formData.length; i++) {
     var name = formData[i].attributes.item(cleanFormTagName).value;
     string += name + '=' +  formData[i].value + '&';
-    callback(formData[i].attributes.item(cleanFormTagName));
+    callback(formData[i].attributes.item(cleanFormTagName), formData[i].value);
   }
   string = string.slice(0, -1);
   return string;
 }
 
 //use the tag to get all the information needed, give the tag a value to give the name to the value.
-gatherFormData('[data-form-information]', (response) => {
+var data = gatherFormData('[data-form-information]', (response, value) => {
   //foreach of the elements a callback gets called with the tag and value as a argument.
-  console.log(response);
+  console.log(response, value);
 });
+
+console.log(data);
