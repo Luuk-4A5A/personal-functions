453e319367a9db4875c6f9fe2149aac0d21cc903
unknown <501287@edu.rocmn.nl>
2017-09-28 10:47:00 +0200

    added javascript function to get all the data with element tags

---
+++ javascript/formdata/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form>
+      <input data-form-information="name" type="text" value="luuk">
+      <input data-form-information="hey" type="text" value="yeah">
+      <textarea data-form-information="textarea">dit is content denk ik</textarea>
+      <select data-form-information="select">
+        <option>1</option>
+        <option selected>2</option>
+        <option>3</option>
+      </select>
+    </form>
+    <script src="main.js"></script>
+  </body>
+</html>
---
+++ javascript/formdata/main.js
@@ -0,0 +1,18 @@
+function gatherFormData(formTagName, callback) {
+  let string = '';
+  let formData = document.querySelectorAll(formTagName);
+  let cleanFormTagName = formTagName.slice(1, -1);
+  for(var i = 0; i < formData.length; i++) {
+    var name = formData[i].attributes.item(cleanFormTagName).value;
+    string += name + '=' +  formData[i].value + '&';
+    callback(formData[i].attributes.item(cleanFormTagName));
+  }
+  string = string.slice(0, -1);
+  return string;
+}
+
+//use the tag to get all the information needed, give the tag a value to give the name to the value.
+gatherFormData('[data-form-information]', (response) => {
+  //foreach of the elements a callback gets called with the tag and value as a argument.
+  console.log(response);
+});
